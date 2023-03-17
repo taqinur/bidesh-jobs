a035297982bc5c74025e34de243b61faf98f06ce
Taqinur <taqi1662@gmail.com>
2023-03-17 23:49:31 +0600

    inital setup done

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            ooot
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+          mohiiiiiiii  
+        </div>
+    );
+};
+
+export default Home;
