e362e7e079d18d582d46c6d2b152346038bc7a87
Taqinur <taqi1662@gmail.com>
2023-03-18 23:08:17 +0600

    header skeleton completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaPhone } from "react-icons/fa";
+
+// import Container from 'react-bootstrap/esm/Container';
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <Navbar bg="" expand="lg">
+            <Container>
+                <img src="	https://bdesh.bdjobs.com/images/logo_bdesh.svg" alt="" width={'170px'} className='ms-0' />
+                <img src="https://bdesh.bdjobs.com/images/Joint_venture.png" alt="" className='mx-auto' />
+                <div><FaPhone /><span className='ms-1'> যোগাযোগ</span></div>
+            </Container>
+        </Navbar>
     );
 };
 
